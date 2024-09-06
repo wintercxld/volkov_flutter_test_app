59c6a68a69b8241e7546ea4d4fcfda17f377d60d
bocchanskyy <nikita_volkov2004@mail.ru>
2024-09-06 18:02:10 +0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/volkov_flutter_test_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.volkov_flutter_test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Волков Никита Андреевич ПИбд-33\n(Flutter Demo Home Page)'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+      
+        
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Вы нажмали на кнопку столько раз:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              if (_counter > 9)
+                Text(
+                  'Хватит кликать! Передохни чуток!'
+                )
+            ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.emoji_nature),
+      ),     );
+  }
+}
